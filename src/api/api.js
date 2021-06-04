113dc0ce066e593d8743c65e8d303645dec210f9
mcheryabushkin <ch.maks@list.ru>
2021-06-04 16:28:48 +0300

    Added api for getting weather of one city

---
+++ src/api/api.js
@@ -0,0 +1,22 @@
+import { KEYS } from './keys';
+
+
+class api {
+    _apiBase = 'http://api.openweathermap.org/data/2.5/';
+
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not get resource ${url}, received ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getOneDayWeather(city) {
+        return this.getResource(`weather?q=${city}&appid=${KEYS.API_KEY}`);
+    }
+}
+
+export default new api();
---
+++ src/api/keys.js
@@ -0,0 +1,3 @@
+export const KEYS = {
+    API_KEY: 'dff6e9743e495997780afa49fb980424'
+}
